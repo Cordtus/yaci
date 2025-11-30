d9635ed45f38774d018138bd423b17954eb4186e
Cordtus <Cordtus@gmail.com>
2025-11-29 17:10:39 -0700

    add YACI_START_HEIGHT env support to entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -11,5 +11,10 @@ if [ "$YACI_INSECURE" = "true" ]; then
   INSECURE_FLAG="--insecure"
 fi
 
+START_FLAG=""
+if [ -n "$YACI_START_HEIGHT" ]; then
+  START_FLAG="-s $YACI_START_HEIGHT"
+fi
+
 # Go code auto-resumes from last indexed block when no -s flag is passed
-exec yaci extract postgres "$YACI_GRPC_ENDPOINT" -p "$YACI_POSTGRES_DSN" --live --enable-prometheus --prometheus-addr 0.0.0.0:2112 -c "${YACI_CONCURRENCY:-5}" $INSECURE_FLAG "$@"
+exec yaci extract postgres "$YACI_GRPC_ENDPOINT" -p "$YACI_POSTGRES_DSN" --live --enable-prometheus --prometheus-addr 0.0.0.0:2112 -c "${YACI_CONCURRENCY:-5}" $INSECURE_FLAG $START_FLAG "$@"
