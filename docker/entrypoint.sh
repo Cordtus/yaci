e2c9b35fdd892c71212a6e37d5b915a85ae0e3c6
Cordtus <Cordtus@gmail.com>
2025-11-29 17:08:31 -0700

    add YACI_INSECURE env support to entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,5 +6,10 @@ if [ -z "$YACI_GRPC_ENDPOINT" ] || [ -z "$YACI_POSTGRES_DSN" ]; then
   exit 1
 fi
 
+INSECURE_FLAG=""
+if [ "$YACI_INSECURE" = "true" ]; then
+  INSECURE_FLAG="--insecure"
+fi
+
 # Go code auto-resumes from last indexed block when no -s flag is passed
-exec yaci extract postgres "$YACI_GRPC_ENDPOINT" -p "$YACI_POSTGRES_DSN" --live --enable-prometheus --prometheus-addr 0.0.0.0:2112 -c "${YACI_CONCURRENCY:-5}" "$@"
+exec yaci extract postgres "$YACI_GRPC_ENDPOINT" -p "$YACI_POSTGRES_DSN" --live --enable-prometheus --prometheus-addr 0.0.0.0:2112 -c "${YACI_CONCURRENCY:-5}" $INSECURE_FLAG "$@"
